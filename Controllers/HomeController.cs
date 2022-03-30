e6d39a2028cee9851436a281c61f44beae9c66c0
csalvesen84 <73505297+csalvesen84@users.noreply.github.com>
2022-03-30 16:13:41 -0600

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private BowlersDbContext _context { get; set; }
+        public HomeController(BowlersDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            List<Team> teams = _context.Teams.ToList();
+
+            List<Bowler> cont = _context.Bowlers.ToList();
+            return View(cont);
+        }
+
+        public IActionResult Team(int TeamId)
+        {
+
+            var Team = _context.Teams.Single(x => x.TeamId == TeamId);
+            ViewBag.TeamName = Team.TeamName;
+
+            List<Team> teams = _context.Teams.ToList();
+
+            List<Bowler> cont = _context.Bowlers.Where(x => x.TeamId == TeamId).ToList();
+            return View(cont);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Teams = _context.Teams.ToList();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Bowler submission)
+        {
+            ViewBag.Teams = _context.Teams.ToList();
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(submission);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int bowlerid)
+        {
+            ViewBag.Teams = _context.Teams.ToList();
+
+            var bowler = _context.Bowlers.Single(x => x.BowlerID == bowlerid);
+
+            return View("Add", bowler);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Bowler submission)
+        {
+            ViewBag.Teams = _context.Teams.ToList();
+
+            if (ModelState.IsValid)
+            {
+                _context.Bowlers.Update(submission);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int bowlerid)
+        {
+            var bowler = _context.Bowlers.Single(x => x.BowlerID == bowlerid);
+
+            return View(bowler);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Bowler bowler)
+        {
+            _context.Bowlers.Remove(bowler);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Models
+{
+    public class Team
+    {
+        [Key]
+        public int TeamId { get; set; }
+        [Required]
+        public string TeamName { get; set; }
+        public int CaptainId { get; set; }
+    }
+}
